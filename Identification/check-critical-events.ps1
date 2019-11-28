84381fb5136e394f6680009a7d0e510bae660402
ikhosravi <im.khosravi@gmail.com>
2019-11-28 12:24:53 +0330

    Update check-critical-events.ps1

--- Identification/check-critical-events.ps1
+++ Identification/check-critical-events.ps1
@@ -10,7 +10,7 @@
 
 Get-WinEvent -FilterHashtable @{LogName="Security"; ID=4624,4720,4722,4724,4738,4732,5140,1102}
 
-Get-WinEvent -FilterHashtable @{LogName="System"; ID=7030,7045,1056,7045,10000,10001,10100,20001,20002,20003,24576,24577,24579}
+Get-WinEvent -FilterHashtable @{LogName="System"; ID=7030,1056,7045,10000,10001,10100,20001,20002,20003,24576,24577,24579}
 
 Get-WinEvent -FilterHashTable @{LogName="Microsoft-Windows-Windows Firewall With Advanced Security/Firewall"; ID=2003}
 
